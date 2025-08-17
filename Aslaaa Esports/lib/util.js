db9eedc5aaba32499029a89221e02ce5de64a543
Abhijeet gautam <aslaaaxsergeant@gmail.com>
2025-08-17 16:30:20 +0530

    Delete Aslaaa Esports directory

--- Aslaaa Esports/lib/util.js
+++
@@ -1,7 +0,0 @@
-// Tiny helper library (no external deps)
-(function(global){
-  const qs = (sel, el=document) => el.querySelector(sel);
-  const qsa = (sel, el=document) => Array.from(el.querySelectorAll(sel));
-  const on = (el, ev, fn) => el.addEventListener(ev, fn);
-  global.$ = { qs, qsa, on };
-})(window);
